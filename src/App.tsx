7abdfea3d6531784a0c0e3a7a2ddc87fe430ff43
Rafael Barbosa Silva <r.ji@outlook.com>
2022-06-27 15:57:12 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/header";
+
+import "./app.css";
 
 function App() {
-  return <>'Hey hoo'</>;
+  return (
+    <>
+      <Header />
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={"header-inner-container"}>
+        <div className={"header-title-area"}>
+          <h1 className={"header-title"}>Wiliot</h1>
+          <p className={"header-subtitle"}>Test</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

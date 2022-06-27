13de1673915b8c87643fabeb6b01d41f73076ac0
Rafael Barbosa Silva <r.ji@outlook.com>
2022-06-27 16:54:52 -0300

    feat: start getting data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/header";
 
 import "./app.css";
 
 function App() {
+  useEffect(() => {
+    const ws = new WebSocket(process.env.REACT_APP_API_URL || "");
+
+    ws.onmessage = (event) => {
+      console.log("hey", JSON.parse(event.data));
+    };
+
+    return () => ws.close();
+  }, []);
+
   return (
     <>
       <Header />
